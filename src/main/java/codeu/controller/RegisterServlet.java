de4784153181640c1efd9f5a81e51c4f3d22e558
Ed <ele2117@columbia.edu>
2018-03-21 23:21:24 -0400

    added doPost method to RegisterServlet

--- src/main/java/codeu/controller/RegisterServlet.java
+++ src/main/java/codeu/controller/RegisterServlet.java
@@ -16,4 +16,15 @@ public class RegisterServlet extends HttpServlet{
     throws IOException, ServletException {
       request.getRequestDispatcher("/WEB-INF/view/register.jsp").forward(request,response);
     }
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws IOException, ServletException {
+
+      String username = request.getParameter("username");
+      String password = request.getParameter("password");
+
+      response.getWriter().println("<p>Username: " + username + "</p>");
+      response.getWriter().println("<p>Password: " + password + "</p>");
+    }
 }
