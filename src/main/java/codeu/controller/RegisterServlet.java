1874f5137c3e3a49445a36fddc2c985e9f6e14a5
Ed <ele2117@columbia.edu>
2018-03-23 04:13:14 -0400

    added UserStore to RegisterServlet

--- src/main/java/codeu/controller/RegisterServlet.java
+++ src/main/java/codeu/controller/RegisterServlet.java
@@ -11,6 +11,29 @@ import javax.servlet.http.HttpServletResponse;
 */
 public class RegisterServlet extends HttpServlet{
 
+  /**
+   * Store class that gives access to Users.
+   */
+  private UserStore userStore;
+
+  /**
+   * Set up state for handling registration-related requests. This method is only called when
+   * running in a server, not when running in a test.
+   */
+  @Override
+  public void init() throws ServletException {
+    super.init();
+    setUserStore(UserStore.getInstance());
+  }
+
+  /**
+   * Sets the UserStore used by this servlet. This function provides a common setup method
+   * for use by the test framework or the servlet's init() function.
+   */
+  void setUserStore(UserStore userStore) {
+    this.userStore = userStore;
+  }
+
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response)
     throws IOException, ServletException {
